704aa453e8fe1abcb155c79723611ae45a25d553
ttymck <tyler@tylerkontra.com>
2019-12-26 14:22:44 -0500

    feat(engine): renderer, scene, actor model - fill in readme

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # procworld
 a procedurally-generated-game engine
+
+## Purpose
+
+Seeks to answer the question: 
+> Can you tell a cohesive, compelling narrative using a procedurally generated story, providing gameplay within that narrative?
+
+Using NLP techniques, a storyline should be able to be sequenced, randomly generated but cohesive (think AI generated news articles).
+
+More difficult is providing art and gameplay for such a randomly generated narrative.
+
+**Hypothesis:** Using public domain images on the web, keywords can be converted to art assets. Furthermore, via [style transfer](https://en.wikipedia.org/wiki/Neural_Style_Transfer), these disparate, randomly selected assets can be normalized to a consistent art style, providing recognizable scenes, prcocedurally generated.
+
+The biggest roadblock is likely to be gameplay. How unconstrained can gameplay be? Can it be as random as the narrative?
--- engine/main.py
+++ engine/main.py
@@ -16,26 +16,56 @@ class Receiver(threading.Thread):
           sink.put(line.strip())
 
 
-class Scene(threading.Thread):
-  def __init__(self, intake=queue.Queue()):
-    super(Scene, self).__init__()
+class Actor:
+  def __init__(self, id, name, appearance):
+    self.id = id
+    self.name = name
+    self.appearance = appearance
+
+  def __repr__(self):
+    return f"{self.id}: {self.name} - {self.appearance}"
+
+
+class ActorRepo:
+  def __init__(self, existing_actors=None):
+    self._actors = existing_actors or []
+
+  def of_id(self, actor_id):
+    return Actor(actor_id, "John Smith", "blue")
+
+
+class Scene:
+  def __init__(self, background: str, actors: List[int]):
+    self.background = background
+    self.actors = actors
+
+  def render(self, actor_repo):
+    actors = list(map(actor_repo.of_id, self.actors))
+    print("Background:", self.background, "\nActors:", actors)
+
+
+class Renderer(threading.Thread):
+  def __init__(self, intake=queue.Queue(), actor_repo=ActorRepo()):
+    super(Renderer, self).__init__()
     self.intake = intake
+    self.actor_repo = actor_repo
 
   def run(self):
     while True:
       nextline = self.intake.get(block = True)
-      print(nextline)
+      sc = Scene(nextline, [1])
+      sc.render(self.actor_repo)
 
 
 class Engine():
   def __init__(self):
-    self._scene = Scene(queue.Queue())
-    sinks = [self._scene.intake,]
+    self._renderer = Renderer(queue.Queue())
+    sinks = [self._renderer.intake,]
     self._receiver = Receiver(sinks)
   
   def run(self):
     self._receiver.start()
-    self._scene.start()
+    self._renderer.start()
 
 
 if __name__ == "__main__":
